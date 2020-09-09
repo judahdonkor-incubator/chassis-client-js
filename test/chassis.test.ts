958b777726f4a5c3c4ddaad2567e30811132df31
Judah Donkor <judahdonkor@gmail.com>
2020-09-09 15:37:36 +0000

    Fix test

---
+++ test/chassis.test.ts
@@ -0,0 +1,8 @@
+// import { Chassis } from '../src';
+
+describe('chassis', () => {
+  it('works', () => {
+    // new Chassis((op, svc) => data => Promise.resolve(3 as any))
+    expect(2).toEqual(2);
+  });
+});
